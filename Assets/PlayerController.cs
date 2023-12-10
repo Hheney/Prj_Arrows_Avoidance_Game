9d579c4875134f8e08f5cf8e08682814e47f6f92
Kim Sang Ju <cheney3193@naver.com>
2023-12-10 21:56:55 +0900

    범위이동 제한 2가지 방법 분할 추가

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //플레이어 컨트롤러 내부에서만 사용하므로 private 접근 지정자 사용
-    //private float fLeftWallXPos = -10.5f;   //왼쪽 벽 X 좌표값
-    //private float fRightWallXPos = 10.5f;   //오른쪽 벽 X 좌표값
+    private float fLeftWallXPos = -10.5f;   //왼쪽 벽 X 좌표값
+    private float fRightWallXPos = 10.5f;   //오른쪽 벽 X 좌표값
     private float fGroundYPos = -3.6f;      //바닥 Y 좌표값
 
     private float fXPosRangeLimit = 0.0f;   //X좌표값 범위 제한
@@ -22,6 +22,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //방법 1. Mathf.Clamp() 메소드 사용
+        
         // 키가 눌렀는지 검출하기 위해서는 Input 클래스의 GetKeyDown 메서드를 사용함
         // 이 메서드는 매개변수로 전달한 키가 눌리는 순간 true를 한 번 반환
         // GetKeyDown 메서드는 지금까지 사용하던 GetMouseButtonDown 메서드와 비슷하므로 쉽게 이해할 수 있을 것
@@ -50,6 +52,9 @@ public class PlayerController : MonoBehaviour
 
         //범위를 벗어나면 제한된 최소 최대값 위치로 초기화
         transform.position = new Vector2(fXPosRangeLimit, fGroundYPos);
+        
+
+        //방법 2. if조건문 사용
 
         /*
          * ArrowController 내부 화살이 화면 밖으로 나가면 자기 자신을 소멸시키는 메소드를 참조하여
@@ -59,32 +64,30 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector2(fLeftWallXPos, fGroundYPos); //왼쪽 벽 위치로 초기화
         }
-        else if(transform.position.x > fRightWallXPos) //만약(플레이어의 X좌표값 > 오른쪽 벽 X좌표값)
-        { 
+        else if (transform.position.x > fRightWallXPos) //만약(플레이어의 X좌표값 > 오른쪽 벽 X좌표값)
+        {
             transform.position = new Vector2(fRightWallXPos, fGroundYPos); //오른쪽 벽 위치로 초기화
         }
-        else
-        {
-            // 키가 눌렀는지 검출하기 위해서는 Input 클래스의 GetKeyDown 메서드를 사용함
-            // 이 메서드는 매개변수로 전달한 키가 눌리는 순간 true를 한 번 반환
-            // GetKeyDown 메서드는 지금까지 사용하던 GetMouseButtonDown 메서드와 비슷하므로 쉽게 이해할 수 있을 것
-            // 키를 누른 순간 : GetKeyDown()
-            // 키를 누르다가 뗀 순간 : GetKeyUp()
-            // 왼쪽 화살표 키가 눌렀을 때 -> GetKeyDown(KeyCode.LeftArrow)
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                // Translate 메서드 : 오브젝트를 현재 좌표에서 인수 값만큼 이동시키는 메서드
-                // 값이 -3 이므로 왼쪽으로 '3' 만큼 움직인다.
-                transform.Translate(-0.18f, 0, 0);
-            }
 
-            // 오른쪽 화살표 키가 눌렀을 때 -> GetKeyDown(KeyCode.RightArrow)
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                // Translate 메서드 : 오브젝트를 현재 좌표에서 인수 값만큼 이동시키는 메서드
-                // 값이 3 이므로 오른쪽으로 '3' 만큼 움직인다.
-                transform.Translate(0.18f, 0, 0);
-            }
+        // 키가 눌렀는지 검출하기 위해서는 Input 클래스의 GetKeyDown 메서드를 사용함
+        // 이 메서드는 매개변수로 전달한 키가 눌리는 순간 true를 한 번 반환
+        // GetKeyDown 메서드는 지금까지 사용하던 GetMouseButtonDown 메서드와 비슷하므로 쉽게 이해할 수 있을 것
+        // 키를 누른 순간 : GetKeyDown()
+        // 키를 누르다가 뗀 순간 : GetKeyUp()
+        // 왼쪽 화살표 키가 눌렀을 때 -> GetKeyDown(KeyCode.LeftArrow)
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Translate 메서드 : 오브젝트를 현재 좌표에서 인수 값만큼 이동시키는 메서드
+            // 값이 -3 이므로 왼쪽으로 '3' 만큼 움직인다.
+            transform.Translate(-0.18f, 0, 0);
+        }
+
+        // 오른쪽 화살표 키가 눌렀을 때 -> GetKeyDown(KeyCode.RightArrow)
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Translate 메서드 : 오브젝트를 현재 좌표에서 인수 값만큼 이동시키는 메서드
+            // 값이 3 이므로 오른쪽으로 '3' 만큼 움직인다.
+            transform.Translate(0.18f, 0, 0);
         }*/
     }
 
