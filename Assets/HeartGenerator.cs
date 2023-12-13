c571fcd17343ea96c70a243c58ae69952a3f755a
Kim Sang Ju <cheney3193@naver.com>
2023-12-13 10:03:16 +0900

    하트 에셋 추가 및 프리팹 생성

---
+++ Assets/HeartGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartGenerator : MonoBehaviour
+{
+    public GameObject gHeartPrefab;
+    GameObject gMove = null;
+
+    float fSpan = 1.0f;
+    float fDelta = 0.0f;
+
+    int nPositiionX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
